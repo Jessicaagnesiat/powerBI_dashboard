65c2a2c0d2f63efbc1767491e19ec6678293e0d3
Jessica Agnesia Tataung <jessicaagnesiat@gmail.com>
2026-02-14 00:55:20 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.components.v1.iframe(
+    "https://app.powerbi.com/view?r=eyJrIjoiZjEyNmY5ZWYtNWFiNS00Y2IzLTk4MGQtODIxYWU3MjlmYmYxIiwidCI6IjgyMTE5NTA1LTdmNTYtNGUxZC04NTlhLWJiZWFjODNhNzhhMCIsImMiOjEwfQ%3D%3D",
+    height=800,
+    scrolling=True
+)
